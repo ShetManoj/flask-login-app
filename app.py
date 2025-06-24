45b7961f9444ce89b4c469662ca9c5193843ccf0
ManojS <manojrsnet@gmail.com>
2025-06-24 13:02:15 +0530

    Switched from text file to MySQL database for user information storage

--- app.py
+++ app.py
@@ -1,25 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for
+import mysql.connector
 
 app = Flask(__name__)
 
-users = {}
-
-def load_users():
-    try:
-        with open("users.txt", "r") as f:
-            for line in f:
-                username,password = line.strip().split(",", 1)
-                users[username] = password
-    except FileNotFoundError:
-        pass
-
-def save_user(username, password):
-    with open ("users.txt", "a") as f:
-        f.write(f"{username},{password}\n")
+db = mysql.connector.connect (
+    host="localhost",
+    user="root",
+    password="manoj1196",
+    database="flaskapp"
+)
+cursor = db.cursor()
 
 @app.route("/")
 def home():
     return redirect("/login")
+
 @app.route("/dashboard/<username>")
 def dashboard(username):
     return render_template("dashboard.html", username=username)
@@ -32,15 +27,18 @@ def signup():
         password = request.form["password"]
         confirm = request.form["confirm"]
 
-        if username in users:
+        cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
+        existing_user = cursor.fetchone()
+
+        if existing_user:
             return render_template("message.html", message="Username already exists", redirect_url=url_for('signup'))
         elif password != confirm:
             return "Password does not match, try again"
         elif username.lower() in password.lower():
             return "Password should not be same as username. Choose strong password"
         else:
-            users[username] = password
-            save_user(username, password)
+            cursor.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, password))
+            db.commit()
             return redirect(url_for('dashboard', username=username))
     return render_template("signup.html")
 
@@ -50,7 +48,10 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
 
-        if username in users and users[username] == password:
+        cursor.execute("SELECT * FROM users WHERE username = %s AND password = %s", (username, password))
+        user = cursor.fetchone()
+
+        if user:
             return redirect(url_for('dashboard', username=username))
         else:
             return render_template("message.html", message="Invalid username or password", redirect_url=url_for('login'))
@@ -58,6 +59,6 @@ def login():
     return render_template("login.html")
 
 if __name__ == "__main__":
-    load_users()
+    
     app.run(debug=True)        
     
